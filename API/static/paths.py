3584bec5af16426ea283b831978e823ebdfca787
zzzzVladimirzzzz <77273306+zzzzVladimirzzzz@users.noreply.github.com>
2023-01-13 10:33:40 +0300

    Update paths.py

--- API/static/paths.py
+++ API/static/paths.py
@@ -1,2 +1,2 @@
-delay_file_json_path = '/Users/vladimirpovarov/TASK_SB/API/data_delay.json'
-forms_json_path = '/Users/vladimirpovarov/TASK_SB/API/data_forms_string.json'
+delay_file_json_path = 'data_delay.json'
+forms_json_path = 'data_forms_string.json'
